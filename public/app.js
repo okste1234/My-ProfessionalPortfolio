29080d69be8e0b3595996aa7b586272f1f28a989
Adekunle Stephen Omorotimi <okste1234@gmail.com>
2023-08-31 14:14:24 +0100

    editing

--- public/app.js
+++ public/app.js
@@ -6,26 +6,26 @@ const navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 if (navToggle) {
     navToggle.addEventListener('click',
-    ()=> navMenu.classList.add('show-menu')
+        () => navMenu.classList.add('show-menu')
     )
 };
 /*===== MENU HIDDEN =====*/
 if (navClose) {
     navClose.addEventListener('click',
-    ()=> navMenu.classList.remove('show-menu')
+        () => navMenu.classList.remove('show-menu')
     )
 };
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav_link")
 navLink.forEach(navLi => navLi.addEventListener('click',
-    ()=> navMenu.classList.remove('show-menu')));
+    () => navMenu.classList.remove('show-menu')));
 
 /*==================== TYPING ANIMATION ====================*/
 
-let typed = new Typed(".typing",{
-    strings : ["Frontend Developer.", "Backend Developer.", "Web Designer.", "Freelancer."],
-    typeSpeed: 120,
+let typed = new Typed(".typing", {
+    strings: ["< A Frontend Developer />", "{ A Shopify Developer }", "{ A MERN Stack Developer }"],
+    typeSpeed: 200,
     backSpeed: 100,
     loop: true,
 });
@@ -35,20 +35,20 @@ const modalViews = document.querySelectorAll(".services_modal");
 const modalBtns = document.querySelectorAll(".services_button");
 const modalCloses = document.querySelectorAll(".services_modal-close");
 
-let modal = function(modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add("active-modal")
 }
 
-modalBtns.forEach((modalBtn, i)=>{
-    modalBtn.addEventListener('click', ()=> 
-    modal(i))
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>
+        modal(i))
 })
 
-modalCloses.forEach(modalClose =>{
-    modalClose.addEventListener('click',()=> 
-    modalViews.forEach(modalView =>{
-        modalView.classList.remove("active-modal")
-    }))
+modalCloses.forEach(modalClose => {
+    modalClose.addEventListener('click', () =>
+        modalViews.forEach(modalView => {
+            modalView.classList.remove("active-modal")
+        }))
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
@@ -57,14 +57,14 @@ let swiper = new Swiper(".portfolio_container", {
     loop: true,
 
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  });
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]")
@@ -72,39 +72,39 @@ const sections = document.querySelectorAll("section[id]")
 function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute("id")
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else{
+        } else {
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
-    
+
 }
 window.addEventListener('scroll', scrollActive)
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
@@ -120,9 +120,9 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
